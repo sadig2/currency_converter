f1f9b61027dcc42a41ce5f4fc0309c15fdf391fb
Sadig Naibbayli <clanzu2@yahoo.com>
2025-03-11 15:01:14 +0100

    Set up sqlalchemy engine
    connect to db
    set up env

---
+++ fastapi-app/core/config.py
@@ -0,0 +1,35 @@
+from pydantic import BaseModel, PostgresDsn
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class RunConfig(BaseModel):
+    host: str = "0.0.0.0"
+    port: int = 3000
+
+
+class ApiPrefix(BaseModel):
+    prefix: str = "/api"
+
+
+class DatabaseConfig(BaseModel):
+    url: PostgresDsn
+    echo: bool = False
+    echo_pool: bool = False
+    max_overflow: int = 50
+    pool_size: int = 10
+
+
+class Settings(BaseSettings):
+    model_config = SettingsConfigDict(
+        env_file=(".env.template", ".env"),
+        case_sensitive=False,
+        env_nested_delimiter="__",
+        env_prefix="APP_CONFIG__",
+    )
+    run: RunConfig = RunConfig()
+    api_prefix: ApiPrefix = ApiPrefix()
+    db: DatabaseConfig
+
+
+settings = Settings()
+print(settings.db.echo)
--- fastapi-app/main.py
+++ fastapi-app/main.py
@@ -1,7 +1,28 @@
+from fastapi.concurrency import asynccontextmanager
 import uvicorn
 from fastapi import FastAPI
 
-app = FastAPI()
+from api import router as api_router
+from core.config import settings
+from core.models import db_helper
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # startup
+    yield
+    # shutdown
+    print("Shutting down...")
+    await db_helper.dispose()
+
+
+main_app = FastAPI(
+    lifespan=lifespan,
+)
+main_app.include_router(api_router, prefix=settings.api_prefix.prefix)
+
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", reload=True, port=5000)
+    uvicorn.run(
+        "main:main_app", reload=True, host=settings.run.host, port=settings.run.port
+    )
